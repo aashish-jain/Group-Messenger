d5056eb3a60a760292e7a2aecca17d7c93e8eb8d
Aashish Jain <aashish_jain@live.com>
2019-02-07 17:34:15 -0500

    Template

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -0,0 +1,49 @@
+package edu.buffalo.cse.cse486586.groupmessenger1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.widget.TextView;
+
+/**
+ * GroupMessengerActivity is the main Activity for the assignment.
+ * 
+ * @author stevko
+ *
+ */
+public class GroupMessengerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_messenger);
+
+        /*
+         * TODO: Use the TextView to display your messages. Though there is no grading component
+         * on how you display the messages, if you implement it, it'll make your debugging easier.
+         */
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        
+        /*
+         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
+         * OnPTestClickListener demonstrates how to access a ContentProvider.
+         */
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
+        
+        /*
+         * TODO: You need to register and implement an OnClickListener for the "Send" button.
+         * In your implementation you need to get the message from the input box (EditText)
+         * and send it to other AVDs.
+         */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
+        return true;
+    }
+}
